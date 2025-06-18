75ccbfaf6d09e89830c982a8c714168754bf53b9
Atanasius Marcello Avta Pendhi Putra <marcello.pendhia@gmail.com>
2025-06-18 17:30:12 +0700

    menambahkan api data transaksi pembelian

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -40,4 +40,6 @@ $routes->get('/checkout', 'TransaksiController::checkout', ['filter' => 'auth'])
 $routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
 $routes->get('get-location', 'TransaksiController::getLocation', ['filter' => 'auth']);
 $routes->get('get-cost', 'TransaksiController::getCost', ['filter' => 'auth']); 
-$routes->get('/faq' ,'Home::faq',['filter' => 'auth']);
+$routes->get('/profile', 'Home::profile', ['filter' => 'auth']);
+$routes->get('/faq' ,'Home::Home',['filter' => 'auth']);
+$routes->resource('api', ['controller' => 'apiController']);
